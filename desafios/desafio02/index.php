51a1ec50d96154b19ee49c6e05f036f94785e6a9
Felipe <felipe.mds10@hotmail.com>
2023-04-03 00:51:27 +0100

    Resolucao
    Resolução do Desafio 02
    Sorteador de números.

---
+++ desafios/desafio02/index.php
@@ -0,0 +1,9 @@
+<?php 
+    $min = 0;
+    $max = 1000;
+
+    $num = rand($min, $max);
+
+    echo "Gerando um número aleatório entre $min e $max... \n";
+    echo "<br>O número gerado foi <strong>$num</strong>";
+?>

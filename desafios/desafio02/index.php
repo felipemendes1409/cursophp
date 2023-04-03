669a65d8393d7bc92fb5680472351b1505fd562b
Felipe <felipe.mds10@hotmail.com>
2023-04-03 11:11:44 +0100

    Update index.php

--- desafios/desafio02/index.php
+++ desafios/desafio02/index.php
@@ -2,7 +2,7 @@
     $min = 0;
     $max = 1000;
 
-    $num = random_int($min, $max);
+    $num = mt_rand($min, $max);
 
     echo "Gerando um número aleatório entre $min e $max... \n";
     echo "<br>O número gerado foi <strong>$num</strong>";

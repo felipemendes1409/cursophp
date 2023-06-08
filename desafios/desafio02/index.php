95d0b6b9bef7c6f3f0b8b77745b5343d3d3f17d8
Felipe <felipe.mds10@hotmail.com>
2023-06-08 22:11:32 +0100

    Desafio02
    Melhorei o código criado inicialmente por mim, adicionei um estilo mais retro.

--- desafios/desafio02/index.php
+++ desafios/desafio02/index.php
@@ -1,9 +1,21 @@
-<?php 
-    $min = 0;
-    $max = 1000;
-
-    $num = mt_rand($min, $max);
-
-    echo "Gerando um número aleatório entre $min e $max \n";
-    echo "<br>O número gerado foi <strong>$num</strong>";
+<?php
+$numeroSorteado = rand(1, 100); //gera um número aleatório usando a função rand() do PHP. Neste caso, o número será um valor entre 1 e 100.*/
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerador de Números Aleatórios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Número Aleatório</h1>
+        <p>O número sorteado é: <?php echo $numeroSorteado; ?></p>
+        <button onclick="window.location.reload();">Gerar Novo Número</button>
+    </main>
+</body>
+</html>
